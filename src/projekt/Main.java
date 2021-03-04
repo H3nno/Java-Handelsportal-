d207e645fda0d2919df8fec192a5e1f65df952a9
H3nno <henningschmidt94@gmail.com>
2021-03-04 11:13:08 +0100

    Excel weiter

--- src/projekt/Main.java
+++ src/projekt/Main.java
@@ -5,7 +5,9 @@ public class Main {
 	  public static void main(String[] args) {
 		    
 		    new LoginGUI();
-		    new WarenkorbGUI();
+		   new WarenkorbGUI();
+		  
+		  	//ReadExcel Hallo = new ReadExcel();
 		  } // end of main
 	
 	
--- src/projekt/ReadExcel.java
+++ src/projekt/ReadExcel.java
@@ -4,7 +4,10 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Iterator;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
@@ -13,16 +16,34 @@ public class ReadExcel {
 
 	
 	
-	public static void test() throws FileNotFoundException {
+	public static void test() throws IOException {
 	File excelFile = new File("Benutzerdaten.xlsx");
 	FileInputStream fis = new FileInputStream(excelFile);
 	
 	
 	XSSFWorkbook workbook = new XSSFWorkbook(fis);
 	
-	XSSFSheet sheet = workbook.getSheetAt("0");
+	XSSFSheet sheet = workbook.getSheetAt(0);
 	
+	Iterator<Row> rowIt = sheet.iterator();
 	
+	while(rowIt.hasNext()) {
+		Row row = rowIt.next();
+		
+		Iterator<Cell> cellIterator = row.cellIterator();
+		
+		while(cellIterator.hasNext()) {
+			
+			Cell cell = cellIterator.next();
+			System.out.println(cell.toString() + ";");
+			
+		}
+			
+		System.out.println("Erfolgreich?");
+	}
+	
+	workbook.close();
+	fis.close();
 	
 	
 	}
