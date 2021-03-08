70e4cc91436c05ad156bd3cd1279f5267ca04362
H3nno <henningschmidt94@gmail.com>
2021-03-08 16:59:23 +0100

    Dateien auslösen funktioniert !!!!

--- src/projekt/Login.java
+++ src/projekt/Login.java
@@ -2,84 +2,112 @@ package projekt;
 
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
-import java.io.FileReader;  
+import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
-
-import com.opencsv.CSVReader; 
 
 
 public class Login {
 
+	static String[][] Speichern;
+	static int WieTief = 0;
+
 	public static void RegistrierungButtonPressed() {
-		
+
 		new RegistrierungsGUI();
-		
-		
+
 	}
-	
-	public static void LoginButtonPressed(){
-		
-		
-		
-		Login.readCSV();
-		
-		
-		
+
+	public static void LoginButtonPressed() {
+
+		Login.DarfLogin();
+
 	}
-	
-	
-	
-	public static void CheckLogin(){
-		
+
+	public static boolean DarfLogin() {
+
 		String Username = LoginGUI.jUsername.getText();
 		String Passwort = LoginGUI.jPasswordField1.getText();
-		
-		
-		
+
+		Login.readCSV();
 	
 		
-	}
+
+
+		for (int i = 1; i < WieTief; i++) {
+			
+			if (Username.equals(Speichern[i][2])) {
+
+				if (Passwort.equals(Speichern[i][3])) {
+
+					System.out.println("Test erfolgreich");
+					return true;
+
+				} else {
+
+					System.out.println("Passwort falsch");
+					return false;
+				}
+
+			}
 	
-	public static void readCSV(){
-		
-		
-	String Pfad = "C:\\Users\\Henning\\git\\Java-Handelsportal\\src\\projekt\\Benutzerdaten.csv";
-	String line;
-	
-	ArrayList<String[]> Liste = new ArrayList<String[]>();
-	
-	try {
-		BufferedReader BR = new BufferedReader(new FileReader(Pfad));
-		
-		while((line = BR.readLine()) != null) {
-			String[] Werte = line.split(";");
-			
-			System.out.println(Werte[1]);
-			
-			
-			
 		}
 		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-	} catch (IOException e) {
-		// TODO Auto-generated catch block
-		e.printStackTrace();
+		System.out.println("Benutzer existiert nicht");
+		return false;
+
+
 	}
+
 	
 	
 	
-} 
+	
+	
+	
+	
+	
+	public static String[][] readCSV() {
+
+		String Pfad = "src\\projekt\\Benutzerdaten.csv";
+		String line;
+
+		try {
+			BufferedReader BR1 = new BufferedReader(new FileReader(Pfad));
+
+			int z = 0;
+
+			while ((line = BR1.readLine()) != null) {
+
+				WieTief++;
+
+			}
+
+			Speichern = new String[WieTief][4];
+
+			BufferedReader BR = new BufferedReader(new FileReader(Pfad));
+
+			while ((line = BR.readLine()) != null) {
+
+				String[] Spalten = line.split(";");
+
+				for (int j = 0; j < 4; j++) {
+					Speichern[z][j] = Spalten[j];
+					//System.out.println("Speichern[" + z + "][" + j + "]: " + Speichern[z][j]);
+				}
+
+				z++;
+
+			}
+			return Speichern;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+
+		}
+		return Speichern;
+
+	}
 
 }
-	
-	
-
