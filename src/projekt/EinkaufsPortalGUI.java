07e2fa63355c91a2ef968913d3012cb0151d9201
H3nno <henningschmidt94@gmail.com>
2021-04-03 17:35:58 +0200

    as

--- src/projekt/EinkaufsPortalGUI.java
+++ src/projekt/EinkaufsPortalGUI.java
@@ -1,20 +1,10 @@
 package projekt;
 
 import java.awt.*;
-import java.awt.color.ColorSpace;
 import java.awt.event.*;
 import java.util.ArrayList;
-import java.util.Vector;
-
 import javax.swing.*;
 import javax.swing.event.*;
-
-
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
-import javax.swing.event.*;
-import javax.swing.border.LineBorder;
 
 /**
  *
