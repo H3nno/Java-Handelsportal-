eb0ba7a0106e034e283484b8cfa5f7e61f45c28d
H3nno <henningschmidt94@gmail.com>
2021-04-12 12:20:38 +0200

    Ware csv und WareInteract

---
+++ src/projekt/InteractWarenListe.java
@@ -0,0 +1,125 @@
+package projekt;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class InteractWarenListe {
+
+	static Ware[] Warenliste = null;
+	static int WieTief = 0;
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////	
+	public static Ware[] readCSV() { // vorübergehend Static
+
+		String Pfad = "src\\projekt\\WarenListe.csv";
+		String line;
+		BufferedReader BR1 = null;
+		BufferedReader BR = null;
+		String[][] Speichern = null;
+		try {
+			BR1 = new BufferedReader(new FileReader(Pfad));
+			int z = 0;
+			while ((line = BR1.readLine()) != null) {
+				WieTief++;
+			}
+			Speichern = new String[WieTief][3];
+			BR = new BufferedReader(new FileReader(Pfad));
+			while ((line = BR.readLine()) != null) {
+				String[] Spalten = line.split(";");
+				for (int j = 0; j < 5; j++) {
+					Speichern[z][j] = Spalten[j];
+				}
+				z++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				BR1.close();
+				BR.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+		Warenliste = new Ware[WieTief];
+
+		for (int i = 0; i < WieTief; i++) {
+
+			String tempName = Speichern[i][0];
+			int tempPreis = Integer.parseInt(Speichern[i][1]);
+			String tempBeschreibung = Speichern[i][2];
+
+			Ware temp = new Ware(tempName, tempPreis, tempBeschreibung);
+
+			Warenliste[i] = temp;
+		}
+		WieTief = 0;
+		return Warenliste;
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void writeCSV(Ware[] Liste) {
+
+		FileWriter writer = null;
+		try {
+			writer = new FileWriter("src\\projekt\\WarenListe.csv");
+			writer.write("");
+			writer.close();
+			writer = new FileWriter("src\\projekt\\WarenListe.csv");
+
+			for (int i = 0; i < Liste.length; i++) {
+
+				writer.append("" + Liste[i].getName());
+				writer.append(";");
+				writer.append("" + Liste[i].getPreis());
+				writer.append(";");
+				writer.append("" + Liste[i].getBeschreibung());
+				writer.append("\n");
+
+			}
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		} finally {
+			try {
+				writer.flush();
+				writer.close();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+
+	}
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static void AusgabeWarenliste() {
+
+		Ware[] AListe = readCSV();
+
+		for (int i = 0; i < AListe.length; i++) {
+			System.out.println("WarenID" + i + " " + AListe[i].getName() + " " + AListe[i].getPreis() + " "
+					+ AListe[i].getBeschreibung());
+
+		}
+
+	}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	public static int StelleArray(String Username) {
+
+		Ware[] Liste = readCSV();
+		int UserID = -1;
+
+		for (int i = 0; i < Liste.length; i++) {
+			if (Username.equals(Liste[i].getName())) {
+				UserID = i;
+			}
+		}
+
+		return UserID;
+
+	}
+}
